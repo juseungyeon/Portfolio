5319bfedc3997b1d5bfe07d8b811d31cb8eca25d
joo2 <iauuai@naver.com>
2021-01-20 03:18:38 +0900

    new

--- js/index.js
+++ js/index.js
@@ -26,7 +26,7 @@ var UIPort = {
         }
         function event02() {
             $('.h-text, .bot-text').css({ 'transform': 'translate3d(0px, ' + -pageYOffset/500 + '%, 0px) skew(0deg, '+pageYOffset/100 +'deg)', 'opacity': 1-pageYOffset/450});
-            $('.scroll').css({'opacity': 1-pageYOffset/450});
+            $('.scroll-wrap').css({'opacity': 1-pageYOffset/450});
             if(wScroll>= $work.offset().top) {
                 $('.h-text').css({ 'transform': 'translate3d(0px, -2%, 0px) skew(0deg, 6deg)', 'opacity': 0});
             }
@@ -96,11 +96,11 @@ var UIPort = {
             tl = new TimelineMax();
             tl.add([
                 TweenMax.staggerTo('header h1, header .work-btn',1,{opacity:1, x:0,ease: Power3.easeOut},0.2),
-                TweenMax.to('.intro .trans',0.7,{x:0,y:0,delay: 0.4,ease: Power3.easeOut})
+                TweenMax.to('.intro .trans-text',0.7,{x:0,y:0,delay: 0.4,ease: Power3.easeOut})
             ])
             .staggerTo('.h-text p, .bot-text p',1.2,{opacity:1,y:0,rotation:0,ease: Power3.easeOut},0.2,"-=0.7")
-            .to('.scroll span',1.2,{width:'100%',ease: Power3.easeOut},"-=0.7")
-            .to('.scroll svg',0.8,{opacity:1},"-=0.5");
+            .to('.scroll-wrap span',1.2,{width:'100%',ease: Power3.easeOut},"-=0.7")
+            .to('.scroll-wrap svg',0.8,{opacity:1},"-=0.5");
 
             t2 = new TimelineMax();
             t2.to('.head-img img, .back a',0.8,{opacity:1,delay: 1})
@@ -121,16 +121,14 @@ var UIPort = {
         }
 
         function event(){
-            for (var i=1; i<=5; i++){
-                var page = 'http://jooosg.cafe24.com/p1/w'+i+'.html';
-                if (window.location.href == page) {
-                    $('body').append("<div class='page-loader'><div class='pl1'></div><div class='pl2'></div><div class='pl3'></div></div>");
-                    $('.page-loader').addClass('visible');
-                }
-                setTimeout(function() {
-                    $('.page-loader').remove();
-                }, 1800);
+            var page = 'http://jooosg.cafe24.com/p1/w';
+            if (page) {
+                $('body').append("<div class='page-loader'><div class='pl1'></div><div class='pl2'></div><div class='pl3'></div></div>");
+                $('.page-loader').addClass('visible');
             }
+            setTimeout(function() {
+                $('.page-loader').remove();
+            }, 1800);
         }
         
         init(obj);
@@ -149,11 +147,11 @@ var UIPort = {
         function event() {
             $open.on('click',function(){
                 $('body').css('overflow-y','hidden');
-                $('.work-list-wrap').addClass('active');
-                var workCount = $('.work-list-wrap>div').length;
+                $('.work-list').addClass('active');
                 $('.close').css( 'pointer-events', 'none' );
+                var workCount = $('.work-list>div').length;
                 for (i=0; i<workCount; i++){
-                    $('.work-list-wrap>div>a').eq(i).children('div').css({'transition': '1.5s '+'0.'+i+'s ease-in-out'});
+                    $('.list-con').eq(i).children('div').css({'transition': '1.5s '+'0.'+i+'s ease-in-out'});
                 }
                 setTimeout(function() {
                        $('.close').css( 'pointer-events', '' );
@@ -178,12 +176,12 @@ var UIPort = {
         function event() {
             $close.on('click',function(){
                 $('body').css('overflow-y','');
-                $('.work-list-wrap').removeClass('active');
+                $('.work-list').removeClass('active');
                 $('.work-btn').css( 'pointer-events', 'none');
-                $('.work-list-wrap').css( 'transition', '0.5s 2s');
+                $('.work-list').css( 'transition', '0.5s 2s');
                 setTimeout(function() {
                     $('.work-btn').css( 'pointer-events', '' );
-                    $('.work-list-wrap').css( 'transition', '' );
+                    $('.work-list').css( 'transition', '' );
                 }, 2300);
             });
         }
@@ -247,6 +245,7 @@ var UIPort = {
             function loadError(){
                 if((navigator.appName == 'Netscape' && navigator.userAgent.search('Trident') != -1) || (agent.indexOf("msie") != -1)){
                     $('body').css('display','none');
+                    alert('IE는 지원하지 않습니다');
                     alert('IE는 지원하지 않습니다');
                 }
             }
